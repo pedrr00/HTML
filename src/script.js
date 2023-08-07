bbdc25a55917fded910b5f76d9fdfcaa8371787f
Pedro Delgado <pedro.d.duartesc@gmail.com>
2023-08-07 20:17:38 +0100

    transformando um item em objeto

--- src/script.js
+++ src/script.js
@@ -6,10 +6,22 @@ document.addEventListener("DOMContentLoaded", function (){
         
         const formData = new FormData(formulario);
 
+        const data = {
+            nome:"",
+            email:"",
+            senha:"",
+            idade:"",
+            genero:"",
+            interesse:"",
+            pais:"",
+            mensagem:"",
+        };
+
         formData.forEach((value, campo) => {
-           console.log(campo + ":" + value);
-            
+            data[campo] = value;            
         });
+
+        console.log(data);
         //logica para processar o formulario
         alert("formulario enviado com sucesso");
     });
